f6782e5f6c4135ae683d5dc6ec5f6896afe2a821
Fernanda Sorrigotti <fernanda.sorrigotti@hotmail.com>
2023-11-03 14:02:56 +0000

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  if (html.classList.contains("light")) {
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando oculos e blusa preta e jaqueta jeans, barba e fundo azul"
+    )
+  } else {
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo , usando oculos e camisa preta, barba e fundo amarelo"
+    )
+  }
+}
